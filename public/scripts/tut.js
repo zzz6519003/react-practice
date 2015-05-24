9405ee70e11097306b13100f01e20afa40e2654a
Zhengzhong <zhengzhongzhao@gmail.com>
2015-05-24 09:01:42 +0800

    haha

---
+++ public/scripts/tut.js
@@ -0,0 +1,10 @@
+var CommentBox = React.createClass({
+  displayName: 'CommentBox',
+  render: function() {
+    return React.createElement('div', {className: "commentBox"},
+      "Hello, world! I am a CommentBox.")
+  }
+})
+
+React.render(React.createElement(CommentBox, null),
+  document.getElementById('content'))
